3d501b0c4e35b463e6fccff8029779a9d53652ec
Cody Milliron <codymilliron.dev@gmail.com>
2025-05-06 22:58:59 -0400

    2_19 Tokenizing text

---
+++ 02-strings-and-text/2_19_Tokenizing_text.py
@@ -0,0 +1,25 @@
+import re
+from collections import namedtuple
+
+NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
+NUM = r'(?P<NUM>\d+)'
+PLUS = r'(?P<PLUS>\+)'
+TIMES = r'(?P<TIMES>\*)'
+EQ = r'(?P<EQ>=)'
+WS = r'(?P<WS>\s+)'
+
+text = 'foo = 23 + 42 * 10'
+master_pat = re.compile("|".join([NAME, NUM, PLUS, TIMES, EQ, WS]))
+
+Token = namedtuple("Token", ["type", 'value'])
+
+def generate_tokens(pat, text):
+    scanner = pat.scanner(text)
+    for m in iter(scanner.match, None):
+        yield Token(m.lastgroup(), m.group())
+
+# Example use
+for tok in generate_tokens(master_pat, 'foo = 42'):
+    print(tok)
+
+    ## This program doesn't work and I need to debug it. 
