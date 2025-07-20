276d72a2ecd7dbffd0b160cf38c84d43986e5e26
Cody Milliron <codymilliron.dev@gmail.com>
2025-07-19 23:41:34 -0400

    selecting random interger

---
+++ 03-numbers-dates-and-times/3-11_picking_things_at_random.py
@@ -0,0 +1,31 @@
+import random
+values = [1, 2, 3, 4, 5, 6]
+print(random.choice(values))
+
+print(random.choice(values))
+
+print(random.choice(values))
+
+print(random.choice(values))
+
+print(random.choice(values))
+
+print(random.sample(values, 2))
+
+print(random.sample(values, 2))
+
+print(random.sample(values, 3))
+
+print(random.sample(values, 3))
+
+random.shuffle(values)
+print(values)
+
+
+for _ in range(0, 10):
+    print(random.randint(0,10))
+
+for _ in range(0, 10):
+    print(random.random())
+
+print(random.getrandbits(200))
