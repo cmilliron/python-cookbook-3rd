3cff23d54ee125646866e9b1e39e3f550e076daa
Cody Milliron <codymilliron.dev@gmail.com>
2025-07-25 16:46:38 -0400

    manually consuming an iterator

---
+++ 04-iterators-and-generators/4_01_manually_consuming_an_iterator.py
@@ -0,0 +1,15 @@
+with open('/etc/passwd') as f:
+    try:
+        while True:
+            line = next(f)
+            print(line, end='')
+    except StopIteration:
+        pass
+    
+print("with next()")
+with open('/etc/passwd') as f:
+     while True:
+         line = next(f, None)
+         if line is None:
+             break
+         print(line, end='')
