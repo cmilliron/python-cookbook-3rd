82cbb3e402eb06fe81f9fd6a04f68cc2337802b0
Cody Milliron <codymilliron.dev@gmail.com>
2025-07-24 21:48:00 -0400

    Manipulating Dates Involving time zone

---
+++ 03-numbers-dates-and-times/3-16_manipulating_dates_involving_time_zones.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from pytz import timezone
+d = datetime(2012, 12, 21, 9, 30, 0)
+print(d)
+
+# Localize the date for Chicago
+central = timezone('US/Central')
+loc_d = central.localize(d)
+print(loc_d)
+
+# Convert to Bangalore time
+bang_d = loc_d.astimezone(timezone('Asia/Kolkata'))
+print(bang_d)
+
+d = datetime(2013, 3, 10, 1, 45)
+loc_d = central.localize(d)
+print(loc_d)
+
+later = loc_d + timedelta(minutes=30)
+print(later + " wrong") # WRONG! WRONG!
+
+from datetime import timedelta
+later = central.normalize(loc_d + timedelta(minutes=30))
+print(later)
