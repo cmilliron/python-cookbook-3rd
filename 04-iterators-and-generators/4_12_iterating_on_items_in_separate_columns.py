e991f2667c49991f3ddfbe2486d176a26927883a
Cody Milliron <codymilliron.dev@gmail.com>
2025-08-07 17:34:05 -0400

    Iterating on Items in Separate Containers"

---
+++ 04-iterators-and-generators/4_12_iterating_on_items_in_separate_columns.py
@@ -0,0 +1,10 @@
+"""You need to perform the same operation on many objects, 
+but the objects are contained in different containers, 
+and you’d like to avoid nested loops without losing the readability of your code.
+"""
+from itertools import chain
+a = [1, 2, 3, 4]
+b = ['x', 'y', 'z']
+for x in chain(a, b):
+    print(x)  
+    
